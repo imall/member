6baa4eafc8989a7d53c1d816a82499a285dce7da
imall <walker@jiapin.org>
2022-12-02 10:21:06 +0800

    'add'

--- middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['checkToken'] = require('..\\middleware\\checkToken.js')
-middleware['checkToken'] = middleware['checkToken'].default || middleware['checkToken']
-
-export default middleware
